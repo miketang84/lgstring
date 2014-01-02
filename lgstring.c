fc3c201117d37f2ccf48565df2984f9df6d3c69e
Daogang Tang <daogangtang@gmail.com>
2014-01-02 10:45:16 +0800

    add tag {_ Signed-off-by: Daogang Tang <daogangtang@gmail.com>

--- lgstring.c
+++ lgstring.c
@@ -11,7 +11,7 @@
 #define TRUE 1
 #define FALSE 0
 
-#define TAGNUM 7
+#define TAGNUM 8
 #define PAIR 2
 
 
@@ -308,6 +308,7 @@ static int splitset(lua_State* L) {
 
 const char tagset[TAGNUM][PAIR][3] = {
 	{ "{{", "}}" },
+	{ "{_", "_}" },
 	{ "{%", "%}" },
 	{ "{(", ")}" },
 	{ "{^", "^}" },
